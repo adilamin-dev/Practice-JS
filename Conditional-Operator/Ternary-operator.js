4452210e2b05d6f28fff48a61dadc4b1c1ca5e9f
adilamin-dev <adilamin.dev@gmail.com>
2022-10-22 18:27:15 +0600

    Complex Ternary operator practice.

--- Conditional-Operator/Ternary-operator.js
+++ Conditional-Operator/Ternary-operator.js
@@ -1,2 +1,10 @@
-const age = 24;
+// const age = 24;
 console.log(age > 18 ? `You are adult.` : `You are child.`);
+
+const myObj = {
+	name: "Adil Amin",
+	age: 24,
+    job: "Frontend web developer."
+};
+
+console.log(myObj.name ? myObj.age ? myObj.job ? `Got all info` : `Job not found.` : `Age not found.` : `Name not found.`);
